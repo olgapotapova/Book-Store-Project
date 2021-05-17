d793990dee7aaef2667ae73952a9deb2f4049361
Karl-Erik Kald <karl80038@gmail.com>
2021-05-17 14:49:32 +0300

    Use a variable instead of the static value in the title tag

--- content/html-begin.php
+++ content/html-begin.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>FashionBook></title>
+    <title><?php echo $title ?></title>
     <meta charset = "UTF-8">
     <link rel = "stylesheet" type = "text/css"
           href = "style/style.css">
