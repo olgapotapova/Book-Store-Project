db3a325a519bd5a8c1ca4c022a4e291e33310f37
olgapotapova <61624978+olgapotapova@users.noreply.github.com>
2021-05-19 12:42:13 +0300

    conection DB

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
     /*The central configuration file
     that contains required parameters to establish a connection with the MySQL Server's database instance */
-    $username = "";
-    $database = "";
-    $password = "";
-    $servername = "";
+    $username = "d86295sa328613";
+    $database = "d86295_bookstore";
+    $password = "ZivuVZ0Ne1D";
+    $servername = "d86295.mysql.zonevs.eu";
     $yhendus = new mysqli($servername, $username, $password, $database);
     /* Use UTF8 encoding for database */
     $yhendus -> set_charset("utf8");
