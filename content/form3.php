044e605397d244b1691a13f9ce15e6c06b9744f6
Sirje0221 <sytiste33@gmail.com>
2021-05-25 22:01:15 +0300

    Add code for newsletter form in shop page

--- content/form3.php
+++ content/form3.php
@@ -1,4 +1,6 @@
-
+<?php if($title == "Fashion Book") { ?>
+	
+	
 	<form action="" method="post" id="form3" class="form3">
 	<h4>Newsletter</h4>
 	<div class="container_form3">
@@ -7,3 +9,15 @@
 		<div class="child_form3"><input type="submit" name="sisestusnupp" value="Subscribe" /></div>
 	</div>
 	</form>
+
+	<?php } else { ?>
+		<form action="" method="post" id="form3" class="form">
+	<h4>Newsletter</h4>
+	<div class="news_form">
+		<input class="src" type="text" name="email" placeholder="Email Address"/>
+		<input class="btn" type="submit" name="sisestusnupp" value="Subscribe" />
+	</div>
+	</form>
+    <?php
+       }
+    ?>
