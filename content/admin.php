c89cf806d754beec5bb082034571f3d769ff3f4a
Karl-Erik Kald <karl80038@gmail.com>
2021-05-26 09:00:55 +0300

    Add the ability to add a new book

--- content/admin.php
+++ content/admin.php
@@ -1,19 +1,45 @@
 <?php 
     $title = "Administration"; //Title for the page.  Used in the <title> tag
     include('html-begin.php');
+	if(!empty($_REQUEST["add_book"]))
+    {
+        $command = $yhendus-> prepare("INSERT INTO books(author, title, price, genre) VALUES (?, ?, ?, ?)");
+        $command -> bind_param("ssis", $_REQUEST["author_field"], $_REQUEST["title_field"], $_REQUEST["price_field"], $_REQUEST["genre_field"]);
+        $command -> execute();
+        header("Location: $_SERVER[PHP_SELF]?page=admin");
+        $yhendus-> close();
+    }
+	if (!empty($_REQUEST["remove_book"]))
+	{
+		$command = $yhendus -> prepare("DELETE FROM books WHERE id = ?");
+		$command -> bind_param("i", $_REQUEST["id"]);
+		$yhendus -> execute();
+		header("Location: $_SERVER[PHP_SELF]?page=admin");
+        $yhendus-> close(); 
+	}
+	$command = $yhendus -> prepare("SELECT id, author, title, price, genre FROM books");
+	$command -> bind_result($id, $author, $title, $price, $genre);
+	$command -> execute();
+	$yhendus -> close();
+
 ?>
 <main>
-	<p>
-	From their opening paragraphs alone, 
-	I'm willing to bet you're persuaded to check out one brand in 
-	more detail over the other. It isn't just the language itself 
-	that gives you a sense of their business -- it's the design, the 
-	font, and the color.
-	Ultimately, your company profile matters. 
-	It can intrigue a new visitor to check out your 
-	products or services in more detail, and nudge potential customers 
-	into choosing your business over competitors.
-	</p>
+	<form action='?'>
+        Author:
+        <input type='text' name="author_field">
+		<br>
+		Title:
+		<input type='text' name="title_field">
+		<br>
+		Price:
+		<input type='text' name="price_field">
+		<br>
+		Genre:
+		<input type='text' name="genre_field">
+		<br>
+        <input type='submit' value='add_book'>
+    </form>
+	
 
 <?php
     include('html-end.php');
