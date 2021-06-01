cb03410bd7f2ac8f388ca5a8eea2036bb7376406
Karl-Erik Kald <karl80038@gmail.com>
2021-06-01 17:20:35 +0300

    It is now possible to remove books, books are now listed on the administration page, adjustments related to adding books

--- content/admin.php
+++ content/admin.php
@@ -1,46 +1,98 @@
 <?php 
-    $title = "Administration"; //Title for the page.  Used in the <title> tag
+	$title = "Administration"; //Title for the page.  Used in the <title> tag
     include('html-begin.php');
-	if(!empty($_REQUEST["add_book"]))
-    {
-        $command = $yhendus-> prepare("INSERT INTO books(author, title, price, genre) VALUES (?, ?, ?, ?)");
-        $command -> bind_param("ssis", $_REQUEST["author_field"], $_REQUEST["title_field"], $_REQUEST["price_field"], $_REQUEST["genre_field"]);
-        $command -> execute();
-        header("Location: $_SERVER[PHP_SELF]?page=admin");
-        $yhendus-> close();
-    }
-	if (!empty($_REQUEST["remove_book"]))
+	
+	if(isSet($_REQUEST['add_book']))
 	{
-		$command = $yhendus -> prepare("DELETE FROM books WHERE id = ?");
-		$command -> bind_param("i", $_REQUEST["id"]);
-		$yhendus -> execute();
-		header("Location: $_SERVER[PHP_SELF]?page=admin");
-        $yhendus-> close(); 
+		$author = $_REQUEST["author_field"];
+		$booktitle = $_REQUEST["title_field"];
+		$price = $_REQUEST["price_field"];
+		$genre = $_REQUEST["genre_field"];
+		$imageURL = $_REQUEST["imageURL_field"];
+		$kask = $dbConnection -> prepare("INSERT INTO books (bookAuthor, bookTitle, bookCost, bookGenre, bookPicture) 
+		VALUES (?,?,?,?,?)");
+		echo $dbConnection -> error;
+		$kask-> bind_param("ssdss", $author, $booktitle, $price, $genre, $imageURL);
+		$kask-> execute();
 	}
-	$command = $yhendus -> prepare("SELECT id, author, title, price, genre FROM books");
-	$command -> bind_result($id, $author, $title, $price, $genre);
-	$command -> execute();
-	$yhendus -> close();
 
+	if (isSet($_REQUEST["remove_book"]))
+	{
+		$command = $dbConnection -> prepare("DELETE FROM books WHERE bookId = ?");
+		$command -> bind_param("i", $_REQUEST["id"]);
+		$command -> execute();
+		// header("Location: $_SERVER[PHP_SELF]?page=admin");
+		$dbConnection -> close();
+	}
 ?>
 <main>
-	<form action='?'>
+	<!-- <form action='?page=admin.php' method='post'> -->
+	<form action='?page=admin.php' method="post">
         Author:
-        <input type='text' name="author_field">
+        <input type='text' name='author_field'/>
 		<br>
 		Title:
-		<input type='text' name="title_field">
+		<input type='text' name='title_field'/>
 		<br>
 		Price:
-		<input type='text' name="price_field">
+		<input type='text' name='price_field'/>
 		<br>
 		Genre:
-		<input type='text' name="genre_field">
+		<input type='text' name='genre_field'/>
 		<br>
-        <input type='submit' value='add_book'>
+		Link to the product image:
+		<input type='text' name='imageURL_field'/>
+		<br>
+        <input type='submit' name='add_book' value='Add'/>
     </form>
-	
+	<?php
 
-<?php
-    include('html-end.php');
-?>
+	?>
+	<?php
+		$items;
+		$cnt = 0;
+		$index = 0; 
+		$command = $dbConnection -> prepare("SELECT bookId, bookAuthor, bookTitle, bookCost, bookGenre, bookPicture FROM books");
+		$command -> bind_result($id, $author, $title, $price, $genre, $picture);
+		$command -> execute();
+		while($command -> fetch())
+		{
+			$book = array($id, $author, $title, $price, $genre);
+			$items[$index] = $book;
+			$cnt += 1;
+			$index += 1;
+		}
+		if ($cnt > 0)
+		{
+			echo "<table>";
+			echo "<th></th>";
+			echo "<th>Title</th>";
+			echo "<th>Author</th>";
+			echo "<th>Genre</th>";
+			echo "<th>Price</th>";
+			foreach ($items as $item)
+			{
+				echo "<tr>";
+				
+				foreach($item as $subitem)
+				{
+					echo "<td>$subitem</td>";
+				}
+				//Delete button
+				echo $item[0];
+				echo "<td><form action='?page=admin.php' method='post'>
+					<input type = 'hidden' name = 'id' value = '$item[0]'/>
+					<input type = 'submit' name = 'remove_book' value = 'Remove'/>
+					</form></td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		}
+		else
+		{
+			echo "There are currently no books in the database.";
+		}
+	?>
+
+
+<?php include('html-end.php');
