3b7a077f575e3686eeca7fff5ad87e366a2deff3
olgapotapova <61624978+olgapotapova@users.noreply.github.com>
2021-06-09 10:23:47 +0300

    buttons

--- content/cart.php
+++ content/cart.php
@@ -1,3 +1,8 @@
+<script type="text/javascript">
+  function togglePic() {
+    file_put_contents('./cart.txt', null)
+  }
+</script>
 <?php 
     $title = "Cart"; //Title for the page.  Used in the <title> tag
     include('html-begin.php');
@@ -6,88 +11,54 @@
 <section>
 <?php
 
-/*$items = fopen( './cart.txt' ,"r");
-while ($line = fgets ($items))
-{*/
-
-
-/*$ides=file('./cart.txt');
-var_dump($ides);
-$ides = file_get_contents('./cart.txt');*/
-
-//$ides = implode(", ", $ides);
-//$ides = explode(',', file_get_contents('./cart.txt'));
-// To check the number of lines
-//echo count($ides).'<br>';
-//foreach($ides as $id){
-//{
-/*    $items;
+require_once('config.php');
+$items;
 $cnt = 0;
 $index = 0;
-    $str = file_get_contents('./cart.txt');
-    $num = preg_match_all("/[0-9]+/", $str, $matches);
-    var_dump($num);
-    $count = count($ides);
 
-  for ($i = 0; $i < $count; $i++) {
- 
+/*$str = file_get_contents('./cart.txt');
+$num = preg_match_all("/[0-9]+/", $str, $matches);*/
 
-$command = $yhendus->prepare("SELECT bookId, author, title, cost, genre, picture FROM Books WHERE bookId IN( . $num . )");//='" . $id . "'");   LIKE %search%
+
+$command = $yhendus->prepare("SELECT bookId, picture, title, author, cost FROM Books");
 if (!$command) {
     die("Connection failed: " . mysqli_connect_error());
-    echo $yhendus -> error;
 }
-$command->bind_param("s", $_REQUEST["id"]);
-
-echo $yhendus -> error;
-$command->bind_result($id, $author, $title, $cost, $genre, $picture);
+$command->bind_result($id, $picture, $title, $author, $cost);
 $command->execute();
+/*session_start();
+$_SESSION['picture'] = $book[1];
+$_SESSION['title'] = $book[2];
+$_SESSION['author'] = $book[3];
+$_SESSION['cost'] = $book[4];
+header("Location: $_SERVER[PHP_SELF]?page=product.php");*/
+
 while ($command->fetch()) {
-    $book = array($id, $author, $title, $genre, $cost);
-   
-
-}
-
-if ($cnt > 0) {
-    echo "<table class='table1'>";
-    echo "<th>ID</th>";
-    echo "<th>Title</th>";
-    echo "<th>Author</th>";
-    echo "<th>Genre</th>";
-    echo "<th>Price</th>";
-    echo "<th>Button</th>";
-    foreach ($items as $item) {
-        echo "<tr>";
     
-        foreach ($item as $subitem) {
-            echo "<td>$subitem</td>";
-        }
+    $book = array($id, $picture, $title, $author, $cost);
+
+    $ides=file('./cart.txt');
+    foreach($ides as $i){
     
-        echo "<td><form action='?page=admin.php' method='post'>
-                <input type = 'hidden' name = 'id' value = '$item[0]'/>
-                <input type = 'submit' name = 'remove_book' value = 'Remove'/>
-                </form></td>";
-        echo "</tr>";
+
+    if (strpos(implode(" ", $book), $i) !== false){
+
+    echo "<div class='cart__container'>";	
+    echo "<form action='?page=cart.php' method='post'>";
+    echo "<holder><div class='cart__child1'><img name = 'picture' src=$picture ></div></holder>";
+    echo "<div class='cart__child2'><br><li><input name='id' id='id' class='hidden' value = '$id' ></input><h4 id = 'tit' name = 'tit'>".  $title ."</h4></li>";
+    echo "<li><h6 id = 'auth' name = 'auth'>".  $author ."</h6></li>";
+    echo "<li>&#x24; <strong name = 'cost'>".  $cost ."</strong><br><br><br><br><br><br><br><br><br><br><br><br></li>";
+    echo "</div>";
     }
-    echo "</table>";
-} else {
-    echo "There are currently no books in the database.";
+    $items[$index] = $book;
+    $cnt += 1;
+    $index += 1;}
+
 }
 
-$items[$index] = $book;
-$cnt += 1;
-$index += 1;
 
-}*/
-
-
-
-
-
-
-
-
-$data = ", ";
+$data = " ";
 $id = $_REQUEST['id'];
 $id = htmlspecialchars($id);
 $id = urldecode($id);
@@ -121,25 +92,29 @@ while ($command->fetch()) {
     $index += 1;
 }
 
+if(isset($_POST['delete']))
+{
+    $fh = fopen('./cart.html', 'a');
+fwrite($fh, ' ');
+fclose($fh);
 
-/*$items[$index] = $book;
-$cnt += 1;
-$index += 1;}
+unlink('../cart.html');
+    //fclose(fopen('./cart.txt','w'));
+    //file_put_contents('./cart.txt', '');
+}
 
-}*/
+echo "<div class='cart__container'>";
+echo "<form onsubmit='togglePic();' action='?page=_main.php' method='post'>";
+echo"<input class='delete' type = 'submit' name ='buy' value='Buy' style='height: 30px'>";
+echo "</div>";
 
-    //session_start();
-    /*echo "<div class='cart__container'>";	
-    echo "<form action='?page=cart.php' method='post'>";
-    echo "<holder><div class='cart__child1'><img  src='$_SESSION[picture]' name = 'picture' ></div></holder>";
-    echo "<div class='cart__child2'><br><li><h4 name = 'title'>". $_SESSION['title'] ."</h4></li>";
-    echo "<li><h6 name = 'author'>". $_SESSION['author'] ."</h6></li>";
-    echo "<li>&#x24; <strong name = 'cost'>". $_SESSION['cost'] ."</strong><input type='number' id='tentacles' name='tentacles' min='1' max='10' placeholder=' 1 pcs'></li>";
-    echo "</form>";
-    echo "</div>";
-    var_dump($_SESSION);
-    die();*/
+echo "<div class='cart__container'>";
+echo "<form onsubmit='togglePic();' action='?page=_main.php' method='post'>";
+echo"<input class='delete' type = 'submit' name ='delete' value='Delete' style='height: 30px'>";
+echo "</div>";
+
 ?>
+
 </section>
 <?php
     include('html-end.php');
